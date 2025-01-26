0453d79fdff5fde89bcbdd7f35cc8090cc4462f4
SamLynch16 <143627552+SamLynch16@users.noreply.github.com>
2025-01-26 16:07:13 -0500

    Update server.js

--- server.js
+++ server.js
@@ -5,16 +5,16 @@ const port = process.env.PORT || 3000;
 
 // Serve client1.html when accessing /client1
 app.get('/client1', (req, res) => {
-  res.sendFile(path.join(Simple-HTTP-Assignment, 'client1.html'));
+  res.sendFile(path.join("Simple-HTTP-Assignment", 'client1.html'));
 });
 
 // Serve client2.html when accessing /client2
 app.get('/client2', (req, res) => {
-  res.sendFile(path.join(Simple-HTTP-Assignment, 'client2.html'));
+  res.sendFile(path.join("Simple-HTTP-Assignment", 'client2.html'));
 });
 
 // Serve static files (like images, CSS, JS)
-app.use(express.static(path.join(Simple-HTTP-Assignment, 'public')));
+app.use(express.static(path.join("Simple-HTTP-Assignment", 'public')));
 
 // Start the server
 app.listen(port, () => {
