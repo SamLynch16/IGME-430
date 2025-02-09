610605051019f6aed2ca471926345bf92ea4cb8a
SamLynch16 <spl8631@rit.edu>
2025-02-09 17:35:34 -0500

    initial commit

---
+++ Simple-HTTP-Assignment-Start-master/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Serve client1.html when accessing /client1
+app.get('/client1', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client1.html')); 
+});
+
+// Serve client2.html when accessing /client2
+app.get('/client2', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client2.html')); 
+});
+
+// Serve static files (like images, CSS, JS)
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
